85f1409cd3488d259c84e9034431eb664c4d589e
GuillaumeBls <xtrem.944@hotmail.fr>
2018-03-23 14:37:59 +0100

    Indexing with elastic search, INSERT, Search..

--- index.js
+++ index.js
@@ -1,21 +1,96 @@
-const {getModels} = require('node-car-api');
-const {getBrands} = require('node-car-api');
+const {
+  getModels
+} = require('node-car-api');
+const {
+  getBrands
+} = require('node-car-api');
+
+var client = require('./connection.js');
 
 var voiture = new Array();
 
-async function store () {
+var json = {
+  brand: "",
+  model: "",
+  volume: "",
+  uuid: "",
+  name: "",
+};
+var brand = voiture;
+
+var models;
+
+async function store() {
   const brands = await getBrands();
-  
+
   var count = 0;
-  
-  brands.forEach(async function(brand){
-    var models = await getModels(brand);
-    console.log(models);
-    voiture[count] = models;
-    
-    count++;
+
+  brands.forEach(async function (brand) {
+    models = await getModels(brand);
+    //console.log(models);
+
+    models.forEach(async function (model) {
+
+      insertData(count, model);
+      count++;
+    })
+
   })
-  
+
 }
 
 store();
+
+// Document add
+function insertData(index, voiture) {
+  client.index({
+    index: 'caradisiac',
+    id: index,
+    type: 'voiture',
+    body: voiture
+  }, function (err, resp, status) {
+    console.log(resp);
+  });
+}
+
+// Document delete
+
+function deleteData(index, voiture) {
+  client.delete({
+    index: 'caradisiac',
+    id: index,
+    type: 'voiture'
+  }, function (err, resp, status) {
+    console.log(resp);
+  });
+}
+
+// Search data
+
+function search() {
+  client.search({
+    index: 'caradisiac',
+    type: 'voiture',
+    body: { //Retirer pour tout voir
+      query: {
+        //match: { "brand": "RENAULT" }
+        "range": {
+          "volume": {
+            "gte": 500
+          }
+        }
+      },
+    }
+  }, function (error, response, status) {
+    if (error) {
+      console.log("search error: " + error)
+    } else {
+      console.log("--- Response ---");
+      console.log(response);
+      console.log("--- Hits ---");
+      response.hits.hits.forEach(function (hit) {
+        console.log(hit);
+      })
+    }
+  });
+}
